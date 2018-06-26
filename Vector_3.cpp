870b6ecbff659de4aadc8ce136d344e2f5ef7e91
DEGGY_MUDI <iyinevb@gmail.com>
2018-06-26 20:04:00 +0100

    Initial Commit of Vector Class

---
+++ Vector_3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Evbuoma Osamudiame_Odegua on 26/06/2018.
+//
+
+#include "Vector_3.h"
---
+++ Vector_3.h
@@ -0,0 +1,22 @@
+//
+// Created by Evbuoma Osamudiame_Odegua on 26/06/2018.
+//
+
+#ifndef CLASS_CREATI_VECTOR_3_H
+#define CLASS_CREATI_VECTOR_3_H
+
+class Vector_3 {
+public:
+    Vector_3():i(0), j(0), k(0)
+    {
+
+    }
+private:
+    float i;
+    float j;
+    float k;
+
+};
+
+
+#endif //CLASS_CREATI_VECTOR_3_H
